6c2eebe332d7e09d61ab4c88054d4d5c3a9c5744
mrc-rdn <johnmarcoardina49@gmail.com>
2025-11-26 12:25:33 +0800

    edit

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,11 @@ const app = express();
 const port = 3000;
   
 const db = new pg.Pool({
-   user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+   connectionString: process.env.DATABASE_URL,
+   ssl: 
+     process.env.NODE_ENV === "production" 
+     ? {rejectUnauthorized: false } 
+     : false, 
 });
 
 
